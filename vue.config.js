196ed674ec28c52fa751462c86da50e59301a783
Ivan <damon.greeb@gmail.com>
2022-08-14 11:44:06 +0200

    sh for deploy

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,9 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
+  publicPath: process.env.NODE_ENV === 'production'
+      ? '/mobytrip/'
+      : '/',
   css: {
     loaderOptions: {
       sass: {
